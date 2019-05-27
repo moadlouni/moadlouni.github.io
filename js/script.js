1f6805ecf8c760c40352f17c69ba914cf7ad1339
Mo Adlouni <modullah@gmail.com>
2019-05-27 15:33:46 -0500

    ready for submission. Website still needs work.

--- js/script.js
+++ js/script.js
@@ -1,85 +1,10 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
 
-    /*window.onload = function aboutImage() {
-        if (window.location.href.indexOf('about.html') > -1){
-            document.body.style.backgroundImage = "url('../images/coffee_berries.jpg')";
-        }
-    } */
-    // $('.send').click(function (event){
-    //     event.preventDefault()
-
-    //     var name = $('name').val()
-    //     var email = $('email').val()
-    //     var message = $('message').val()
-    //     varStatusElm = $
-
-
-    
-    // })
-
-    //     var name = document.getElementsByClassName("form-group")[0].id;
-    //     document.getElementById("name").innerText = name;
-
-    //     var email = document.getElementsByClassName("form-group")[1].id;
-    //     document.getElementById("email").innerHTML = x;
-        
-    //     var x = document.getElementsByClassName("form-group")[0].id;
-    //     document.getElementById("name").innerHTML = x;
-
     jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000
 
 })
 
-// $(function() {
-//     // Get the form.
-//     var form = $('#ajax-contact');
-
-//     // Get the messages div.
-//     var formMessages = $('#form-messages');
-
-//     // Set up an event listener for the contact form.
-//     $(form).submit(function(event) {
-//         // Stop the browser from submitting the form.
-//         event.preventDefault();
-
-//             // Serialize the form data.
-//         var formData = $(form).serialize();
-//         // Submit the form using AJAX.
-//         $.ajax({
-//             type: 'POST',
-//             url: $(form).attr('action'),
-//             data: formData
-//         })
-
-//         .done(function(response) {
-//             // Make sure that the formMessages div has the 'success' class.
-//             $(formMessages).removeClass('error');
-//             $(formMessages).addClass('success');
-        
-//             // Set the message text.
-//             $(formMessages).text(response);
-        
-//             // Clear the form.
-//             $('#name').val('');
-//             $('#email').val('');
-//             $('#message').val('');
-//         })
-
-//         .fail(function(data) {
-//             // Make sure that the formMessages div has the 'error' class.
-//             $(formMessages).removeClass('success');
-//             $(formMessages).addClass('error');
-        
-//             // Set the message text.
-//             if (data.responseText !== '') {
-//                 $(formMessages).text(data.responseText);
-//             } else {
-//                 $(formMessages).text('Oops! An error occured and your message could not be sent.');
-//             }
-//         });
-//     });
-// });
 document.addEventListener('DOMContentLoaded', bindButtons);{
 
     function bindButtons(){
